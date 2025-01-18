e1623b76603cc9821166ecf990d548d6b7ed7b8c
Darren+Satkunas <darren@satkunas.com>
2025-01-18 14:33:37 +0000

    initial commit

---
+++ theme.js
@@ -0,0 +1,12 @@
+// https://tailwindcss.com/docs/font-family#customizing-your-theme
+export const theme = {
+    extend: {
+        fontFamily: {
+            'neutra': 'Neutra',
+            'neutra-condensed': 'NeutraCondensed',
+            'neutra-thin': 'NeutraThin'
+        }
+    }
+}
+
+export default theme
